da489164b0faf2d208cfe12130b132b962f219df
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2025-04-24 17:53:59 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://application:1234@activityapp.9v5yxwx.mongodb.net/?retryWrites=true&w=majority&appName=activityapp");
+    await mongoose.connect('mongodb+srv://itbafa:itbafa1234@cluster0.krvdnxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
