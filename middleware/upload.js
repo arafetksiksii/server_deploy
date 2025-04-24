c49b6a96caa01d634c6fa5c078c93985d8e06f4d
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2025-04-24 11:06:30 +0100

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,15 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinaryConfig"); // Adjust the path if needed
+const cloudinary = require("../config/cloudinaryConfig");
 
-// Cloudinary storage config
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: "events", // Cloudinary folder name
-    allowed_formats: ["jpg", "jpeg", "png"],
+  params: async (req, file) => ({
+    folder: "events",
+    format: "png", // force a format or use: path.extname(file.originalname).substring(1)
+    public_id: file.originalname.split(".")[0], // name without extension
     transformation: [{ width: 800, height: 600, crop: "limit" }],
-  },
+  }),
 });
 
 const upload = multer({ storage });
