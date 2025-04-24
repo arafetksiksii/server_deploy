1a539b1abe649361b5611ad9e0a3a99e14ec2f8b
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2025-04-24 17:37:43 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb+srv://application:1234@activityapp.9v5yxwx.mongodb.net/?retryWrites=true&w=majority&appName=activityapp");
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
