f5887b074accdc19be5cca2682d093324433a013
arafetksiksii <arafet.ksiksi@esprit.tn>
2025-05-20 22:42:57 +0100

    delete

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -8,3 +8,18 @@ exports.getAllNotifications = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+// DELETE a notification by ID
+exports.deleteNotification = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedNotification = await Notification.findByIdAndDelete(id);
+
+    if (!deletedNotification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -4,5 +4,7 @@ const notificationController = require("../controllers/notificationController");
 
 // GET /notifications
 router.get("/", notificationController.getAllNotifications);
+// DELETE /notifications/:id
+router.delete("/:id", notificationController.deleteNotification);
 
 module.exports = router;
